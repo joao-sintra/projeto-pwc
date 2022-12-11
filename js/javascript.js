6b7b1a52258f677b69623b9990701dae2ed31911
rubenamaral <2220848@my.ipleiria.pt>
2022-12-11 18:48:32 +0000

    Funcionalidade de pesquisa

--- js/javascript.js
+++ js/javascript.js
@@ -9,7 +9,7 @@ for (const icon of icons) {
     icon.src = "img/favorito_preto.png";
   });
 }
-function colocaMaiuscula(str){
+function colocaMaiuscula(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
@@ -19,21 +19,29 @@ let weatherC = {
   "apiKey": "3b465f1655e28cd3f0c1d9d517a22955",
   fetchWeather: function (cidade) {
     fetch(
-      "http://api.openweathermap.org/data/2.5/weather?q="+ cidade +"&units=metric&lang=pt&appid=" + this.apiKey
-      )
+      "http://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&units=metric&lang=pt&appid=" + this.apiKey
+    )
       .then((response) => response.json())
       .then((data) => this.displayWeather(data));
   },
-  displayWeather: function(data) {
+  displayWeather: function (data) {
     const { name } = data;
     const { icon, description } = data.weather[0];
     const { temp } = data.main;
 
-    document.querySelector("#cidade"+name).innerHTML = name;
-    document.querySelector("#temp"+name).innerHTML = temp + " ºC";
-    document.querySelector("#imagem"+name).src = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
-    document.querySelector("#clima"+name).innerHTML= colocaMaiuscula(description);
+
+    document.querySelector("#cidade" + name).innerHTML = name;
+    document.querySelector("#temp" + name).innerHTML = temp + " ºC";
+    document.querySelector("#imagem" + name).src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector("#clima" + name).innerHTML = colocaMaiuscula(description);
   },
+  fetchCities: function (cidade) {
+    fetch(
+      "http://api.openweathermap.org/geo/1.0/direct?q=" + cidade + "&limit=5&appid=" + this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
 };
 
 weatherC.fetchWeather("Lisboa");
@@ -42,6 +50,23 @@ weatherC.fetchWeather("Sagres");
 weatherC.fetchWeather("Lagos");
 weatherC.fetchWeather("Porto");
 weatherC.fetchWeather("Leiria");
+
+
+/*-------pesquisa das cidades à escolha do utilizador (variáveis e constantes)-----*/
+const searchBtn = document.querySelector("#search-addon");
+
+
+searchBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  weatherC.fetchCities(document.querySelector("#textbox").value)
+
+
+
+
+
+});
+
+
 
 /*--------------//------//---------------*/
 
@@ -75,4 +100,6 @@ weatherC.fetchWeather("Leiria");
 // weatherF.fetchWeather("Leiria");
 
 /*--------------//------//---------------*/
+
+
 
