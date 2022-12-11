18ab400eac76379beff8aa82944fbcdb000eb7c8
franciscofurtado <2220870@my.ipleiria.pt>
2022-12-11 01:20:25 +0000

    11/12/2022 - Terminado previsão 6 cidades

--- js/javascript.js
+++ js/javascript.js
@@ -9,3 +9,70 @@ for (const icon of icons) {
     icon.src = "img/favorito_preto.png";
   });
 }
+function colocaMaiuscula(str){
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+/*----- Funções para vizualizar a metereologia das 6 cidades da HOME page com unidade métrica (ºC) -----*/
+
+let weatherC = {
+  "apiKey": "3b465f1655e28cd3f0c1d9d517a22955",
+  fetchWeather: function (cidade) {
+    fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q="+ cidade +"&units=metric&lang=pt&appid=" + this.apiKey
+      )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function(data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp } = data.main;
+
+    document.querySelector("#cidade"+name).innerHTML = name;
+    document.querySelector("#temp"+name).innerHTML = temp + " ºC";
+    document.querySelector("#imagem"+name).src = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+    document.querySelector("#clima"+name).innerHTML= colocaMaiuscula(description);
+  },
+};
+
+weatherC.fetchWeather("Lisboa");
+weatherC.fetchWeather("Faro");
+weatherC.fetchWeather("Sagres");
+weatherC.fetchWeather("Lagos");
+weatherC.fetchWeather("Porto");
+weatherC.fetchWeather("Leiria");
+
+/*--------------//------//---------------*/
+
+/*----- Funções para vizualizar a metereologia das 6 cidades da HOME page com unidade imperial (ºF) -----*/
+// let weatherF = {
+//   "apiKey": "3b465f1655e28cd3f0c1d9d517a22955",
+//   fetchWeather: function (cidade) {
+//     fetch(
+//       "http://api.openweathermap.org/data/2.5/weather?q="+ cidade +"&units=imperial&lang=pt&appid=" + this.apiKey
+//       )
+//       .then((response) => response.json())
+//       .then((data) => this.displayWeather(data));
+//   },
+//   displayWeather: function(data) {
+//     const { name } = data;
+//     const { icon, description } = data.weather[0];
+//     const { temp } = data.main;
+
+//     document.querySelector("#cidade"+name).innerHTML = name;
+//     document.querySelector("#temp"+name).innerHTML = temp + " ºF";
+//     document.querySelector("#imagem"+name).src = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+//     document.querySelector("#clima"+name).innerHTML= colocaMaiuscula(description);
+//   },
+// };
+
+// weatherF.fetchWeather("Lisboa");
+// weatherF.fetchWeather("Faro");
+// weatherF.fetchWeather("Sagres");
+// weatherF.fetchWeather("Lagos");
+// weatherF.fetchWeather("Porto");
+// weatherF.fetchWeather("Leiria");
+
+/*--------------//------//---------------*/
+
