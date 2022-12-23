5a9c0ad9578980017b05ca39902c1b205c95d9e5
João Sintra <2220865@my.ipleiria.pt>
2022-12-23 02:27:06 +0000

    23/12/2022 - Pagina de detalhes a funcionar

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,7 @@
+//Constantes
+
+
+
 const icons = document.getElementsByClassName("icon-favorito");
 for (const icon of icons) {
   icon.addEventListener("mouseover", function handleClick(event) {
@@ -9,92 +13,12 @@ for (const icon of icons) {
     icon.src = "img/favorito_preto.png";
   });
 }
-function colocaMaiuscula(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
+
 
 /*----- Funções para vizualizar a metereologia das 6 cidades da HOME page com unidade métrica (ºC) -----*/
 
-let weatherC = {
-  apiKey: "3b465f1655e28cd3f0c1d9d517a22955",
-  fetchWeather: function (cidade) {
-    fetch(
-      "http://api.openweathermap.org/data/2.5/weather?q=" +
-        cidade +
-        "&units=metric&lang=pt&appid=" +
-        this.apiKey
-    )
-      .then((response) => response.json())
-      .then((data) => this.displayWeather(data));
-  },
-  displayWeather: function (data) {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp } = data.main;
-
-    document.querySelector("#cidade" + name).innerHTML = name;
-    document.querySelector("#temp" + name).innerHTML = temp + " ºC";
-    document.querySelector("#imagem" + name).src =
-      "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-    document.querySelector("#clima" + name).innerHTML =
-      colocaMaiuscula(description);
-  },
-  /*-------Mostrar os dados das cidades em [Array] à escolha do utilizador (Autocomplete-Box)-----*/
-  fetchCities: function (cidade) {
-    fetch(
-      "http://api.openweathermap.org/geo/1.0/direct?q=" +
-        cidade +
-        "&limit=5&appid=" +
-        this.apiKey
-    )
-      .then((response) => response.json())
-      .then((data) => this.displaycities(data));
-  },
-  displaycities: function (data) {
-    let cidades = ""
-    let pais = ""
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].name) {
-        cidades = cidades + data[i].name + " " + "(" + data[i].country + ")" + "<br>"
-      }
-    }
-    document.querySelector(".autocom-box").innerHTML = cidades;
-  }
-  
-};
-weatherC.fetchWeather("Lisboa");
-weatherC.fetchWeather("Faro");
-weatherC.fetchWeather("Sagres");
-weatherC.fetchWeather("Lagos");
-weatherC.fetchWeather("Porto");
-weatherC.fetchWeather("Leiria");
 
 
-/*-------pesquisa das cidades à escolha do utilizador, conforme o que é escrito na (#textbox)-----*/
-
-$("#search-addon").click(function (e) {
-  e.preventDefault();
-
-});
-
-$("#textbox").keyup(function (event) {//Cada vez que o utilizador der release na tecla, ao escrever, é efetuada a pesquisa na API.
-  $(".autocom-box").show();
-  if (event.target.value === "") {
-    $(".autocom-box").hide();
-  }
-
-  weatherC.fetchCities(document.querySelector("#textbox").value)//Pesquisar todos os id's "#textbox", pegando no que o utilizador escreve, pra depois procurar na API
-
-});
-
-
-/*-------Quando o utilzador clica fora da (Autocom-box) ela fecha.-----*/
-document.addEventListener("click", function ClickOutsideBox(event) {
-  const box = document.getElementById("autocom-box");
-  if (event.target.value !== box) {
-    $(".autocom-box").hide();
-  }
-});
 
 
 
---
+++ js/script6cidades.js
@@ -0,0 +1,36 @@
+const apiKey = "3b465f1655e28cd3f0c1d9d517a22955";
+
+function colocaMaiuscula(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+let weather6Cidades = {
+  fetchWeather: function (cidade) {
+    fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+        cidade +
+        ",pt&units=metric&lang=pt&appid=" +
+        apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp } = data.main;
+
+    document.querySelector("#cidade" + name).innerHTML = name;
+    document.querySelector("#temp" + name).innerHTML = temp + " ºC";
+    document.querySelector("#imagem" + name).src =
+      "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector("#clima" + name).innerHTML =
+      colocaMaiuscula(description);
+    console.log(name, temp);
+  },
+};
+weather6Cidades.fetchWeather("Lisboa");
+weather6Cidades.fetchWeather("Faro");
+weather6Cidades.fetchWeather("Sagres");
+weather6Cidades.fetchWeather("Lagos");
+weather6Cidades.fetchWeather("Porto");
+weather6Cidades.fetchWeather("Leiria");
